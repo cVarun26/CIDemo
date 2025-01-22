42c8896ac4914375a0217d80375ee51a920076d3
C Varun <cvarun@Cs-MacBook-Air.local>
2025-01-22 18:46:51 +0530

    initial commit

---
+++ task_manager.py
@@ -0,0 +1,19 @@
+class TaskManager:
+    def __init__(self):
+        self.tasks={}
+    
+    def add_task(self, task_id, task_name):
+         if task_id in self.tasks:
+             raise ValueError("Task already exists")
+         self.tasks[task_id] = {"name":task_name,"status":False}
+
+    def get_task(self, task_id):
+        if task_id not in self.tasks:
+            raise KeyError("Task not found")
+        return self.tasks[task_id]
+    
+    def mark_completed(self, task_id):
+        if task_id not in self.tasks:
+            raise KeyError("Task not found")
+        self.tasks[task_id]["status"]=True
+        return self.tasks[task_id]
