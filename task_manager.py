084210c8616cfddecd85849efe4ba6a092639ab2
C Varun <cvarun561@gmail.com>
2025-01-22 19:23:35 +0530

    commit

--- task_manager.py
+++ task_manager.py
@@ -5,7 +5,7 @@ class TaskManager:
     def add_task(self, task_id, task_name):
          if task_id in self.tasks:
              raise ValueError("Task already exists")
-         self.tasks[task_id] = {"name":task_name,"status":False}
+         self.tasks[task_id] = {"name":task_name,"completed":False}
 
     def get_task(self, task_id):
         if task_id not in self.tasks:
@@ -15,5 +15,5 @@ class TaskManager:
     def mark_completed(self, task_id): #function
         if task_id not in self.tasks:
             raise KeyError("Task not found")
-        self.tasks[task_id]["status"]=True
+        self.tasks[task_id]["completed"]=True
         return self.tasks[task_id]
